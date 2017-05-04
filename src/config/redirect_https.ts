cd4a39c879f749a1b7f87a72bc94b5b1f2079cda
Kieve Chua <kievechua@gmail.com>
2017-05-04 10:34:15 +0800

    Initial commit

---
+++ src/config/redirect_https.ts
@@ -0,0 +1,6 @@
+// https://git.daplie.com/Daplie/node-redirect-https#options
+export default {
+  port: 443,
+  body: '<!-- Hello! Please use HTTPS instead -->',
+  trustProxy: true // default is false
+}
